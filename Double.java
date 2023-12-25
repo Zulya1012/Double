0ba5b4421e7c0ebddb006c0cca75c63ec6c5fa27
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-25 16:50:13 +0400

    Update Double.java

--- Double.java
+++ Double.java
@@ -1,10 +1,10 @@
 public class Double {
-   
-    public static void main(String[] args) {
-           
-        double x = 8.8;
-        double y = 1.5;
-        double z = x - y;
-        System.out.println(z);
-    }
+
+  public static void main(String[] args) {
+
+    double x = 8.8;
+    double y = 1.5;
+    double z = x - y;
+    System.out.println(z);
+  }
 }
